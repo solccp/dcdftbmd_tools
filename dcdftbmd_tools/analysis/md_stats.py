aeec45fb5e4957f81265263d8c7b246ef7712c08
Chien-Pin Chou <sol.chou@gmail.com>
2019-03-22 18:06:39 +0900

    init commit: md_stats tool

---
+++ dcdftbmd_tools/analysis/md_stats.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import statistics
+
+
+def md_stats():
+    parser = argparse.ArgumentParser(description='Stats for MD properties')
+    parser.add_argument('-s', '--start', default=0, type=int)
+    parser.add_argument('-e', '--end', default=-1, type=int)
+    parser.add_argument('-p', '--property', choices=['temp', 'mdenergy', 'kenergy', 'potenergy'], default='temp')
+    parser.add_argument('-m', '--merged', action='store_true')
+    parser.add_argument('filename', metavar='filename', type=str)
+    parser.set_defaults(merged=False)
+    opt = parser.parse_args(sys.argv[1:])
+
+    if (not opt.filename):
+        parser.print_usage()
+        sys.exit(1)
+
+    data=[]
+    if (opt.merged):
+        with open(opt.filename, 'r') as f:
+            for line in f:
+                if (line.startswith('#')):
+                    continue
+                if opt.property == 'temp':
+                    value = float(line.split()[1])
+                    data.append(value)
+                elif opt.property == 'mdenergy':
+                    value = float(line.split()[4])
+                    data.append(value)
+                elif opt.property == 'kenergy':
+                    value = float(line.split()[3])
+                    data.append(value)
+                elif opt.property == 'potenergy':
+                    value = float(line.split()[2])
+                    data.append(value)
+    else:
+        with open(opt.filename, 'r') as f:
+            for line in f:
+                if opt.property == 'temp' and 'TEMPERATURE' in line:
+                    value = float(line.split()[2])
+                    data.append(value)
+                elif opt.property == 'mdenergy' and 'TOTAL MD ENERGY' in line:
+                    value = float(line.split()[4])
+                    data.append(value)
+                elif opt.property == 'kenergy' and 'KINETIC ENERGY' in line:
+                    value = float(line.split()[3])
+                    data.append(value)
+                elif opt.property == 'potenergy' and ('Final' in line and 'DFTB' in line and 'Energy' in line) :
+                    value = float(line.split()[4])
+                    data.append(value)
+                #    data = list(map(float, [line.split()[0] for line in f]))
+
+    real_data = data[opt.start:opt.end]
+
+
+    print ()
+    stddev = statistics.stdev(real_data)
+    if 'energy' in opt.property:
+        print ('{:<6s} {:15s} {:15s} {:15s} {:15s}'.format('N', 'Mean', 'Max', 'Min', 'StdDev(Kcal/mol)'))
+        stddev *= 627.5095
+    else:
+        print ('{:<6s} {:15s} {:15s} {:15s} {:15s}'.format('N', 'Mean', 'Max', 'Min', 'StdDev(K)'))
+
+    print ('-'*70)
+    print('{:<6d} {:<15.8f} {:<15.8f} {:<15.8f} {:<15.8f}'.format( len(real_data), statistics.mean(real_data), min(real_data), max(real_data), stddev))
+
+    print ()
+    #print(statistics.median(real_data), statistics.pstdev(real_data))
